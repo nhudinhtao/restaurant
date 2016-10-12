23dcbdeaea01e66aecf6f94c9b07be69425278cb
%%GITNAME%% <%%GITEMAIL%%>
2016-10-12 18:24:22 +0700

    fix link contact us

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :food_items
+  get 'contact_us' => 'home#contact_us'
 
   root 'home#index'	
   # The priority is based upon order of creation: first created -> highest priority.
