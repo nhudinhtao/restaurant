6a67ee7ae098728270cbf17c54d317c5a163699a
%%GITNAME%% <%%GITEMAIL%%>
2016-10-12 22:38:36 +0700

    implement sort menu items by alphabetical, price low to high and price high to low

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,21 @@
 class HomeController < ApplicationController
+  @orderby = 0
   def index
-    if params[:section] 
-      puts params[:section]
-      @foodItems = FoodItem.where(section: params[:section])
+    section = params[:section]
+    @orderby = params[:orderby] 
+    if section
+        @foodItems = FoodItem.where(section: section)
     else
         @foodItems = FoodItem.all
     end
+    case @orderby.to_i 
+      when 0
+        @foodItems = @foodItems.order('name asc')
+      when 1
+        @foodItems = @foodItems.order('price asc')
+      when 2
+        @foodItems = @foodItems.order('price desc')
+      end
   end
+
 end
