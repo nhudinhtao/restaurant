76f109c4231fd686d3e2024b5bf7df03da502e51
Tao Nhu <%%GITEMAIL%%>
2016-10-21 16:34:54 +0700

    add style for oder

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :food_item
+
+  validates_presence_of :food_item_id
+  validates_presence_of :name
+  validates_presence_of :quantity
+  validates_presence_of :phone
+  validates_presence_of :email
+  validates_presence_of :address
+
 end
